bde4e4e1b12650409fb07e30a785215beadd2ce1
Tooomaass06 <tvalderrama23@alumnos.utalca.cl>
2026-04-23 10:39:26 -0400

    se agrega persistencia en la pagina cuando se recarga ademas de navegacion entre registro, inicio y la pagina principal

--- proy_constr/frontend/src/pages/Home.jsx
+++ proy_constr/frontend/src/pages/Home.jsx
@@ -1,15 +1,25 @@
 import { Header } from '../components/Header'
 import  Footer  from '../components/Footer'
+import './Home.css'
 
 
 
-export default function Home({ user }) {
+export default function Home({ user, onLogout, onGoHome }) {
   return (
     <div>
       <Header />
-      <h1>Hola {user.name}</h1> 
-      {/* contenido */}
-      
+      <div className="home-container">
+        <h1>Hola {user.name}</h1> 
+        {/* contenido */}
+        <div className="home-buttons">
+          <button onClick={onLogout} className="home-logout-btn">
+            Cerrar sesión
+          </button>
+          <button onClick={onGoHome} className="home-home-btn">
+            Inicio
+          </button>
+        </div>
+      </div>
       <Footer />
     </div>
   )
