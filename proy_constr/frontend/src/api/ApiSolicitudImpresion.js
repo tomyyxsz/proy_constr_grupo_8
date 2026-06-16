e1fdfb6e3a8f62e0cf2e64105eaa94b75ccf3722
Tomas Corvalán <tcorvalan23@alumnos.utalca.cl>
2026-06-15 22:18:59 -0400

    Log API base URL in ApiSolicitudImpresion.js
    Added a console log to display the API base URL.

--- proy_constr/frontend/src/api/ApiSolicitudImpresion.js
+++ proy_constr/frontend/src/api/ApiSolicitudImpresion.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API_BASE_URL = `${import.meta.env.VITE_API_URL || "http://localhost:3001"}/api/solicitudes`;
-
+console.log(API_BASE_URL);
 export async function crearSolicitudImpresion({
   idEstudiante,
   color1,
