ad447071eaeb5d370a73f5151eb90e09a36a12ed
tomy <tcorvalan23@alumnos.utalca.cl>
2026-06-18 10:22:18 -0400

    Test 8 para pruebas de backend

--- proy_constr/backend/src/tests/integration/gestionSolicitud.test.js
+++ proy_constr/backend/src/tests/integration/gestionSolicitud.test.js
@@ -103,7 +103,7 @@ describe("gestión de solicitudes de impresión", () => {
 
   it("validar que no hay solicitudes", async () => {
     // borrar todas las solicitudes
-    await prisma.$executeRaw`TRUNCATE TABLE "Usuario" CASCADE;`;
+    await prisma.impresion.deleteMany();
     const response = await request(app).get("/");
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty("message");
