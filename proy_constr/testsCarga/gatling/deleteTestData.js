e1762ebe473b482b2b6248042dd6485543121dd2
tomyyxsz <tomascorvalancsm@gmail.com>
2026-07-02 23:24:01 -0400

    Se agrega al repositorio la libreria userEvent para test, ademas de borrar comentarios en testsCarga/gatling/deleteTestData.js

--- proy_constr/testsCarga/gatling/deleteTestData.js
+++ proy_constr/testsCarga/gatling/deleteTestData.js
@@ -1,42 +1,5 @@
 // funcion para borrar usuarios de prueba generados en la simulacion
 
-/*
-  const borrarUsuarios = scenario("Borrar Usuarios")
-    .feed(crearFeederUsuarios(vu, "registro"))
-    .exec(
-      group("Borrar Usuarios").on(
-        http("Eliminar usuario")
-          .delete((session) => `/api/usuarios/rut/${session.get("rut")}`)
-          .check(status().is(200)),
-      ),
-    );
-
-        .exec (
-      group("Obtener solicitudes del usuario").on(
-        http("Obtener solicitudes del usuario")
-          .get((session) => `/api/impresiones/estudiante/${session.get("usuarioId")}`)
-          .check(status().is(200))
-          .check(jsonPath("$.solicitudes[0].idImpresion").saveAs("idSolicitud")),
-      ),
-    )
-    .exitHereIfFailed()
-    .exec (
-      group ("Borrar solicitudes del usuario").on (
-        http("Borrar solicitudes del usuario")
-          .delete((session) => `/api/impresiones/borrar/${session.get("idSolicitud")}`)
-          .check(status().is(200)),
-      )
-    )
-    .exitHereIfFailed()
-    .exec (
-      group ("Borrar usuario del sistema").on (
-        http("Borrar usuario del sistema")
-          .delete((session) => `/api/usuarios/rut/${session.get("rut")}`)
-          .check(status().is(200)),
-      )
-    )
-
-*/
 import dotenv from "dotenv";
 
 dotenv.config({
